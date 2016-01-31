4bc6cde25df1f2afa6e1151a3770da55198fda54
William Grullon <william.grullon@g3hd.com>
2016-01-31 16:59:37 +0000

    login working

--- app/app.js
+++ app/app.js
@@ -1,12 +1,11 @@
 'use strict';
-
-// Declare app level module which depends on views, and components
+ 
 angular.module('myApp', [
   'ngRoute',
-  'myApp.view1',
-  'myApp.view2',
-  'myApp.version'
+  'myApp.home'
 ]).
 config(['$routeProvider', function($routeProvider) {
-  $routeProvider.otherwise({redirectTo: '/view1'});
+     $routeProvider.otherwise({ 
+         redirectTo: '/home'
+    });
 }]);
