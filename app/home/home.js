d03e9911fd4666c847b23c0da7646e24ac4b2b36
William Grullon <william.grullon@g3hd.com>
2016-02-01 01:58:52 +0000

    auth working

--- app/home/home.js
+++ app/home/home.js
@@ -1,7 +1,8 @@
 'use strict';
- 
-angular.module('myApp.home', ['ngRoute'])
- 
+
+//View
+angular.module('myApp.home', ['ngRoute','firebase'])
+
 //route 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
@@ -11,6 +12,27 @@ angular.module('myApp.home', ['ngRoute'])
 }])
  
 // controller
-.controller('HomeCtrl', [function() {
- 
+.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
+    //firebase user db
+    var firebaseObj = new Firebase("https://angularpress.firebaseio.com");
+    var loginObj = $firebaseSimpleLogin(firebaseObj);
+
+    //model
+    $scope.user = {};
+    $scope.SignIn = function(e){
+        e.preventDefault();
+        var username = $scope.user.email;
+        var password = $scope.user.password;
+        loginObj.$login('password', {
+                email: username,
+                password: password
+            })
+            .then(function(user) {
+                //Success callback
+                console.log('Authentication successful');
+            }, function(error) {
+                //Failure callback
+                console.log('Authentication failure');
+            });
+    }
 }]);
