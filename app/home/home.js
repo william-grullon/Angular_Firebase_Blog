5827cfc9dd7b1676a23a390b24f7bce380772da7
William Grullon <william.grullon@g3hd.com>
2016-02-01 03:14:36 +0000

    users can register

--- app/home/home.js
+++ app/home/home.js
@@ -1,38 +1,47 @@
 'use strict';
 
-//View
 angular.module('myApp.home', ['ngRoute','firebase'])
 
-//route 
 .config(['$routeProvider', function($routeProvider) {
     $routeProvider.when('/home', {
         templateUrl: 'home/home.html',
         controller: 'HomeCtrl'
     });
 }])
- 
-// controller
-.controller('HomeCtrl', ['$scope','$firebaseSimpleLogin',function($scope,$firebaseSimpleLogin) {
-    //firebase user db
-    var firebaseObj = new Firebase("https://angularpress.firebaseio.com");
-    var loginObj = $firebaseSimpleLogin(firebaseObj);
 
-    //model
+.controller('HomeCtrl', ['$scope','$location','CommonProp','$firebaseAuth',function($scope,$location,CommonProp,$firebaseAuth) {
+    var firebaseObj = new Firebase("https://angularpress.firebaseio.com");
+    var loginObj = $firebaseAuth(firebaseObj);
+
     $scope.user = {};
-    $scope.SignIn = function(e){
+    $scope.SignIn = function(e) {
         e.preventDefault();
         var username = $scope.user.email;
         var password = $scope.user.password;
-        loginObj.$login('password', {
+        loginObj.$authWithPassword({
                 email: username,
                 password: password
             })
             .then(function(user) {
                 //Success callback
                 console.log('Authentication successful');
+                CommonProp.setUser(user.password.email);
+                $location.path('/welcome');
             }, function(error) {
                 //Failure callback
                 console.log('Authentication failure');
             });
     }
-}]);
+}])
+.service('CommonProp', function() {
+    var user = '';
+
+    return {
+        getUser: function() {
+            return user;
+        },
+        setUser: function(value) {
+            user = value;
+        }
+    };
+});
--- app/register/register.js
+++ app/register/register.js
@@ -3,17 +3,36 @@
  */
 'use strict';
 
-angular.module('myApp.register', ['ngRoute'])
+angular.module('myApp.register', ['ngRoute','firebase'])
 
-    // route
-    .config(['$routeProvider', function($routeProvider) {
-        $routeProvider.when('/register', {
-            templateUrl: 'register/register.html',
-            controller: 'RegisterCtrl'
-        });
-    }])
+.config(['$routeProvider', function($routeProvider) {
+    $routeProvider.when('/register', {
+        templateUrl: 'register/register.html',
+        controller: 'RegisterCtrl'
+    });
+}])
 
-    // controller
-    .controller('RegisterCtrl', [function() {
-
-    }]);
+.controller('RegisterCtrl', ['$scope','$location','$firebaseAuth', function($scope,$location,$firebaseAuth) {
+    $scope.mesg = 'Hello';
+    var firebaseObj = new Firebase("https://angularpress.firebaseio.com");
+    var auth = $firebaseAuth(firebaseObj);
+    $scope.signUp = function() {
+        if (!$scope.regForm.$invalid) {
+            var email = $scope.user.email;
+            var password = $scope.user.password;
+            if (email && password) {
+                auth.$createUser(email, password)
+                    .then(function() {
+                        // do things if success
+                        console.log('User creation success');
+                        $location.path('/home');
+                    }, function(error) {
+                        // do things if failure
+                        console.log(error);
+                        $scope.regError = true;
+                        $scope.regErrorMessage = error.message;
+                    });
+            }
+        }
+    };
+}]);
